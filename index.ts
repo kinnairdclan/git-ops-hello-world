b6ce309de7a28a8a14020ed58c11da1ee3776b6a
Wynn <wynn.chaney@wanco.com>
2026-01-23 10:57:51 -0700

    changed again

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World from George and team!');
+    res.send('Hello World from Wynn and team!');
 });
 
 app.listen(port, () => {
